7fdd1f749455375f8bf559a5a4e9a4ebc4d3069d
Rad Azzouz <rad@agilebits.com>
2016-10-17 13:46:19 -0400

    Initial Commit

---
+++ Main app with iMessage Extension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Main app with iMessage Extension
+//
+//  Created by Rad Azzouz on 2016-10-17.
+//  Copyright © 2016 BuddyBuild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
